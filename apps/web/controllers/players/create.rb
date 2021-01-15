d456673dd99726da21df19705d7043c47a56b36d
tristandruyen <tristandruyen@pm.me>
2021-01-15 20:55:08 +0100

    Add rake task

--- apps/web/controllers/players/create.rb
+++ apps/web/controllers/players/create.rb
@@ -7,15 +7,13 @@ module Web
         include Web::Action
 
         def call(params)
-          params = get_player_data(params[:player][:ett_id_or_name])
+          params = EttAPI.fetch_player(params[:player][:ett_id_or_name])
           PlayerRepository.new.create(params)
 
           redirect_to '/leaderboard'
         end
 
-        def get_player_data(ett_id)
-          EttAPI.fetch_player(ett_id)
-        end
+        def get_player_data(ett_id); end
       end
     end
   end
