8d4731ec843ff639d929ca5f7c776620ae6eebae
tristandruyen <tristandruyen@pm.me>
2021-01-15 14:49:11 +0100

    Add sidekiq

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require 'hanami/setup'
 require 'hanami/model'
 require_relative '../lib/ett_de'
 require_relative '../apps/web/application'
+require_relative './sidekiq'
 
 Hanami.configure do
   mount Web::Application, at: '/'
