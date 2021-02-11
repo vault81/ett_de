7743d91fee4aa14659ba852f7adcc30c1240fbd7
tristandruyen <tristandruyen@vault81.de>
2021-02-11 01:23:46 +0100

    Sort by tournament rank

--- apps/web/controllers/leaderboard/index.rb
+++ apps/web/controllers/leaderboard/index.rb
@@ -45,7 +45,7 @@ module Web
         def league_ordered_players
           players = PlayerRepository.new.all_in_order(:ett_elo)
           players.sort_by do |player|
-            [player.tournaments.first&.short_name, -player.ett_elo.to_i]
+            [player.tournaments.first&.rank, -player.ett_elo.to_i]
           end
         end
       end
