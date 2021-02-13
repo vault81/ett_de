6b1b53a0648f151c8085cf7d7e9084654d4745fe
tristandruyen <tristandruyen@vault81.de>
2021-02-13 06:10:30 +0100

    wip

--- apps/web/controllers/tournaments/show.rb
+++ apps/web/controllers/tournaments/show.rb
@@ -3,12 +3,21 @@ module Web
     module Tournaments
       class Show
         include Web::Action
+        ID_MAPPING={
+          7 => 1,
+          8 => 2,
+          9 => 3,
+          10 => 4,
+          11 => 5,
+          12 => 6,
+        }
 
         expose :tournament
 
         def call(params)
+          mapped_id=ID_MAPPING[params[:id]
           @tournament =
-            TournamentRepository.new.find_with_relations(params[:id])
+            TournamentRepository.new.find_with_relations(mapped_id || params[:id])
         end
       end
     end
